4efd72718156296f1ec7786bd4e796f2af60b74b
Saad Kashif <saad220905@gmail.com>
2024-07-21 23:30:38 -0400

    implement the home user navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/ui/HeaderBox'
 
 const DashBoard = () => {
+  const loggedIn = { firstName: 'Saad' };
+
   return (
-    <div>DashBoard</div>
+    <section className='Home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions'
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
