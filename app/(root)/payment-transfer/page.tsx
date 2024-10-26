5c22e388290718e8ab8c90bacdb285dd4d147948
Saad Kashif <saad220905@gmail.com>
2024-10-26 17:24:07 -0400

    implement transaction page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,9 +1,17 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const PaymentTransfer = () => {
   return (
     <section className='payment-transfer'>
+      <HeaderBox
+        title='Payment Transfer'
+        subtext='Please provide any specific details or notes related to the payment transfer'
+      />
 
+      <section className='size-full pt-5'>
+
+      </section>
     </section>
   )
 }
